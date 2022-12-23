1ebaf93520bfe4cfc37c37eb41286ce2baaf39f0
César Eduardo de Souza <cesarsouza.2002@icloud.com>
2022-12-23 00:51:43 -0300

    Initial Commit

---
+++ imc_calculator/imc_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  imc_calculatorApp.swift
+//  imc_calculator
+//
+//  Created by César de Souza on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct imc_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
