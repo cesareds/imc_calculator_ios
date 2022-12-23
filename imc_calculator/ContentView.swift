4730a6f1e182cc4ca116cef23cbee183d8c9bc73
César Eduardo de Souza <cesarsouza.2002@icloud.com>
2022-12-23 02:29:35 -0300

    Initial Commit

--- imc_calculator/ContentView.swift
+++ imc_calculator/ContentView.swift
@@ -8,19 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var nome: String
+    @State var altura: String
+    @State var peso: String
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView{
+            VStack {
+                Image(systemName: "figure.arms.open")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Calculadora de IMC")
+                TextField("Nome", text: $nome)
+                TextField("Altura", text: $altura)
+                TextField("Peso", text: $peso)
+                let altura = Float(altura) ?? 0
+                let peso = Float(peso) ?? 0
+                let imc = peso / pow(altura, 2)
+                let resultado = resultado(imc: imc)
+                Text("\(nome) tem IMC " + String(imc))
+                Text("\(nome) está em " + resultado)
+            }
+            .padding()
+                .tabItem{
+                    Image(systemName: "house.fill")
+                    Text("Principal")
+                }.tag(0)
+            VStack{
+                Text("Histórico")
+            }
+            .padding()
+                .tabItem{
+                    Image(systemName: "tray.full")
+                    Text("Historico")
+                }.tag(1)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(nome: "", altura: "", peso: "")
     }
 }
+
+
--- imc_calculator/imc_calculatorApp.swift
+++ imc_calculator/imc_calculatorApp.swift
@@ -11,7 +11,26 @@ import SwiftUI
 struct imc_calculatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(nome: "", altura: "", peso: "")
         }
+    }
+}
+func resultado(imc: Float)->String{
+    if imc < 16{
+        return "em magreza grave"
+    }else if imc < 17{
+        return "em magreza moderada"
+    }else if imc < 18.5{
+        return "em magreza leve"
+    }else if imc < 25{
+        return "saudável"
+    }else if imc < 30{
+        return "sobrepeso"
+    }else if imc < 35{
+        return "com obesidade Grau I"
+    }else if imc < 40{
+        return "com obesidade Grau II (severa)"
+    }else{
+        return "com obesidade Grau III (mórbida)"
     }
 }
